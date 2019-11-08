a4287027813b11f5c3a566638040737a9d7380f8
Leon Lin <leonl@stanford.edu>
2019-11-07 19:41:16 -0800

    TNEAT made

---
+++ Data/create_tneanet.py
@@ -0,0 +1,63 @@
+import sqlite_process as sp
+import snap
+TN = snap.TNEANet
+
+
+
+
+def create_tneanet():
+    meta, matches = sp.loadPickle()
+    numNodes = len(matches) + len(meta.player) + len(meta.team) + len(meta.country)
+    numEdges = 3 * len(matches) + 4 * len(meta.player) + len(meta.team)
+    G = TN.New(numNodes, numEdges)
+    countryToNId = {}
+    teamToNId = {}
+    playerToNId = {}
+    matchIndToNId = []
+    i = 0
+    for (countryId, countryName) in meta.country.items():
+        ni = G.GetNI(G.AddNode(i))
+        G.AddStrAttrDatN(ni, "country", "kind")
+        G.AddIntAttrDatN(ni, countryId, "countryId")
+        G.AddStrAttrDatN(ni, countryName, "countryName")
+        countryToNId[countryId] = i
+        i += 1
+    for (teamId, d) in meta.team.items():
+        ni = G.GetNI(G.AddNode(i))
+        G.AddStrAttrDatN(ni, "team", "kind")
+        G.AddIntAttrDatN(ni, teamId, "teamId")
+        G.AddStrAttrDatN(ni, d['name'], "teamName")
+        teamToNId[teamId] = i
+        EId = G.AddEdge(i, countryToNId[d['country']])
+        G.AddStrAttrDatE(EId, "team from", "kind")
+        i += 1
+    for (playerId, d) in meta.player.items():
+        ni = G.GetNI(G.AddNode(i))
+        G.AddStrAttrDatN(ni, "player", "kind")
+        G.AddIntAttrDatN(ni, playerId, "playerId")
+        G.AddStrAttrDatN(ni, d['name'], "playerName")
+        playerToNId[playerId] = i
+        for teamId in d['team']:
+            EId = G.AddEdge(i, teamToNId[teamId])
+            G.AddStrAttrDatE(EId, "plays for", "kind")
+        i += 1
+    for match in matches:
+        matchIndToNId.append(i)
+        ni = G.GetNI(G.AddNode(i))
+        G.AddIntAttrDatN(ni, match.away_goal, "away_goal")
+        G.AddIntAttrDatN(ni, match.home_goal, "away_goal")
+        G.AddIntAttrDatN(ni, match.stageId, "stageId")
+        G.AddStrAttrDatN(ni, match.season, "season")
+        G.AddIntAttrDatN(ni, match.leagueId, "leagueID")
+        EId = G.AddEdge(i, teamToNId[match.home_team])
+        G.AddStrAttrDatE(EId, "home team", "kind")
+        EId = G.AddEdge(i, teamToNId[match.away_team])
+        G.AddStrAttrDatE(EId, "away team", "kind")
+        EId = G.AddEdge(i, countryToNId[match.countryId])
+        G.AddStrAttrDatE(EId, "match in", "kind")
+        i+=1
+    return G
+
+
+
+
--- Data/sqlite_process.py
+++ Data/sqlite_process.py
@@ -1,31 +1,29 @@
 import sqlite3
-dbfile = 'database.sqlite'
+import pickle
+import os
+sqlGetTable = "SELECT * FROM %s" # Select all from a table
+dbFileName = 'database.sqlite'
 dbSaveFileName = 'soccer_%s.txt'
+pickleFileName = 'soccer.pkl'
 
-commands = [
-    "SELECT name FROM sqlite_master WHERE type='table';",  # list of table names, in 1-tuples
-    "SELECT * FROM %s",  # Select all from a table
-]
 
 class Match:
-    """
-    Stuff not Added
-    id;	league_id;	season;	date;
-    ;goal;	shoton;	shotoff;	foulcommit;	card;	cross;	corner;
-    possession;	B365H;	B365D;	B365A;	BWH;	BWD;	BWA;	IWH;
-        IWD;	IWA;	LBH;	LBD;	LBA;	PSH;	PSD;	PSA;
-        WHH;	WHD;	WHA;	SJH;	SJD;	SJA;	VCH;	VCD;
-        VCA;	GBH;	GBD;	GBA;	BSH;	BSD;	BSA
-    """
     def __init__(self, row):
+        """
+        Stuff not Added
+        id;	goal;	shoton;	shotoff;	foulcommit;	card;	cross;	corner;
+        possession;	B365H;	B365D;	B365A;	BWH;	BWD;	BWA;	IWH;
+            IWD;	IWA;	LBH;	LBD;	LBA;	PSH;	PSD;	PSA;
+            WHH;	WHD;	WHA;	SJH;	SJD;	SJA;	VCH;	VCD;
+            VCA;	GBH;	GBD;	GBA;	BSH;	BSD;	BSA
+        """
         self.id = row['match_api_id']
-        # location
+        # Location
         self.countryId = row['country_id']
-        self.stageId = row['stage']
-        # results
+        # Results
         self.home_goal = row['home_team_goal']
         self.away_goal = row['away_team_goal']
-        # teams
+        # Teams
         self.home_team = row['home_team_api_id']
         self.away_team = row['away_team_api_id']
         self.home_players = [row['home_player_1'], row['home_player_2'],
@@ -36,46 +34,52 @@ class Match:
              row['away_player_3'], row['away_player_4'], row['away_player_5'],
              row['away_player_6'], row['away_player_7'], row['away_player_8'],
              row['away_player_9'], row['away_player_10'], row['away_player_11']]
+        # Time
+        self.leagueId = row['league_id']
+        self.season = row['season']
+        self.stageId = row['stage']
+        # Misc
+        self.date = row['date'] # I think this is the date of the datapoint creation
 
 class MetaData:
     def __init__(self):
         # country ID -> country name
         self.country = {}
-        # stage ID -> country ID
-        self.stage = {}
-        # team ID -> {'name': string team name, 'stage': home stage ID, 'country' : home country ID}
+        # team ID -> {'name': string team name, 'country' : home country ID}
         self.team = {}
-        # player ID -> {'name': string team name, 'bday': birthday, 'h': height, 'w': weight}
+        # player ID -> {'name': string name, 'team': list of teamID, 'bday': birthday, 'h': height, 'w': weight}
         self.player = {}
 
     def AddCountry(self, c_row):
         self.country[c_row['id']] = c_row['name']
 
-    def _AddStage(self, match):
-        if match.stageId is None or match.countryId is None:
-            return
-        if match.stageId in self.stage and self.stage[match.stageId] != match.countryId:
-            print("Stage error %d, in country %d and %d" % (match.stageId, self.stage[match.stageId], match.countryId))
-        else:
-            self.stage[match.stageId] = match.countryId
-
-    def _AddTeambyMatch(self, match):
-        # add checks for conflicts
-        if match.home_team is not None:
-            if match.home_team not in self.team:
-                self.team[match.home_team] = {}
-            if match.stageId is not None:
-                self.team[match.home_team]['stage'] = match.stageId
-            if match.countryId is not None:
-                self.team[match.home_team]['country'] = match.countryId
-
     def AddMatchInfo(self, match):
-        self._AddStage(match)
-        self._AddTeambyMatch(match)
+        htId, cId = match.home_team, match.countryId
+        if htId is None:
+            print("Match %d has no home team?" % match.id)
+            return
+        if htId not in self.team:
+            self.team[htId] = {'name': '', 'country': cId}
+            return
+        ht = self.team[htId]
+        if cId is not None:
+            if ht['country'] not in (-1, cId):
+                print("Team/Country error: Team %d at home in country %d and %d" % (htId, ht['country'], cId))
+            ht['country'] = cId
+        for pl in match.home_players:
+            if pl is None:
+                continue
+            if pl not in self.player:
+                print("Player %d not in players table" % pl)
+                continue
+            if htId not in self.player[pl]['team']:
+                self.player[pl]['team'].append(htId)
+
 
     def AddPlayer(self, p_row):
         self.player[p_row['player_api_id']] = {
             'name': p_row['player_name'],
+            'team': [],
             'bday': p_row['birthday'],
             'h': p_row['height'],
             'w': p_row['weight'],
@@ -85,23 +89,26 @@ class MetaData:
         name = t_row['team_long_name']
         id = t_row['team_api_id']
         if id not in self.team:
-            self.team[id] = {'name': name, 'stage': -1, 'country': -1}
+            self.team[id] = {'name': name, 'country': -1}
         else:
             self.team[id]['name'] = name
 
-
-
-
+""" 
+    Initial exploration, figure out what tables there are and -> dbSaveFileName
+    Other stuff I learned: "stage" is how far into a league a match is
+        each team plays 38 games in a league, so "stage" in [1, 38] is the number
+        of that game
+"""
 def getTablesInfo():
-    connection = sqlite3.connect(dbfile)
+    connection = sqlite3.connect(dbFileName)
     connection.row_factory = sqlite3.Row
     c = connection.cursor()
-    c.execute(commands[0])
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
     ret = c.fetchall()
     tableNames = [r['name'] for r in ret]
     lines = []
     for tableName in tableNames:
-        c.execute(commands[1] % tableName)
+        c.execute(sqlGetTable % tableName)
         ret = c.fetchall()
         keys = ret[0].keys()
         lines.append("Table: %s\n" % tableName)
@@ -111,14 +118,43 @@ def getTablesInfo():
     with open(dbSaveFileName % "tables", 'a+') as f:
         f.writelines(lines)
 
-def main():
-    connection = sqlite3.connect(dbfile)
+# create a MetaData obj and list of matches from the SQLite file, save to pickle
+def exploreSQL(saveToPickle = True):
+    connection = sqlite3.connect(dbFileName)
     connection.row_factory = sqlite3.Row
     c = connection.cursor()
 
-    # get matches
-    c.execute(commands[1] % "Match")
-    ret = c.fetchall()
+    meta = MetaData()
+    c.execute(sqlGetTable % "Team")
+    [meta.AddTeam(row) for row in c.fetchall()]
+    
+    c.execute(sqlGetTable % "Player")
+    [meta.AddPlayer(row) for row in c.fetchall()]
+    
+    c.execute(sqlGetTable % "Country")
+    [meta.AddCountry(row) for row in c.fetchall()]
+
+    c.execute(sqlGetTable % "Match")
+    matches = [Match(r) for r in c.fetchall()]
+    [meta.AddMatchInfo(match) for match in matches]
+
+    if saveToPickle:
+        with open(pickleFileName, 'wb+') as f:
+            pickle.dump([meta, matches], f)
+
+    return meta, matches
+
+# load the MetaData obj and list of matches from the pickle file
+def loadPickle():
+    if not os.path.exists(pickleFileName):
+        print("Error: %s does not exist in directory" % pickleFileName)
+        return None
+    with open(pickleFileName, 'rb') as f:
+        return pickle.load(f)
+
+
+
+
 
 
 
